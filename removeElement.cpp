0d3c7a6afcd68ad507015f61d76e1bdc201304f2
Sunny Dutta <sunnyrizzler123@gmail.com>
2026-02-09 23:39:56 +0530

    Create removeElement.cpp

---
+++ removeElement.cpp
@@ -0,0 +1,1 @@
+//Given an integer array arr[] and an integer element, the task is to remove the occurence of element from arr[] in place and return the number of elements which are not equal to element.
